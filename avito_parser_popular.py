d37c0d28a6d061d89626c4c8fa7b4606b77243d0
Timur Salakhetdinov <stimur55@gmail.com>
2024-03-31 10:04:45 +0300

    modified:   avito_parser_popular.py

--- avito_parser_popular.py
+++ avito_parser_popular.py
@@ -30,13 +30,17 @@ def load_links(brand):
     return links
 
 # List of brands for which you have corresponding text files
-brands = ['audi', 'bmw', 'changan', 'chery', 'chevrolet', 'exeed']
-          #'ford', 'geely', 'haval', 'honda', 'hyundai', 'kia', 'mazda',  
+# brands = ['audi', 'bmw', 'changan', 'chery', 'chevrolet', 'exeed']
+brands = ['chery',] 
+          #'ford', 'geely', 'haval', 'honda', 'hyundai', 'kia', 'mazda',]  
           #'mercedes-benz', 'mitsubishi', 'nissan', 'omoda', 'opel', 'renault', 'skoda', 'toyota', 'volkswagen', 'gaz', 'vaz_lada']
 
 # Function to parse brands from the Avito website
 def avito_parser_popular(brand, links, limit=None):
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
     options.add_argument("start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
@@ -143,13 +147,14 @@ def avito_parser_popular(brand, links, limit=None):
     driver.quit()
 
     # Save to an Excel file specific to the brand
-    save_to_excel(offers, brand)
+    save_to_csv(offers, brand)
 
-# Function to save data to Excel, named after the brand
-def save_to_excel(offers, brand):
+# Function to save data to CSV, named after the brand
+def save_to_csv(offers, brand):
     df = pd.DataFrame(offers)
-    filename = f'avito_cars_{brand}.xlsx'
-    df.to_excel(filename, index=False)
+    # Update the filename extension to '.csv'
+    filename = f'avito_cars_{brand}.csv'
+    df.to_csv(filename, index=False)
     print(f"Data for {brand} saved to {filename}")
 
 if __name__ == "__main__":
